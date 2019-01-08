59b7fc9736e128c424224150d70370e2218c7382
feng.wen <feng.wen@yuneec.com>
2019-01-08 22:07:04 +0800

    each frame add pcr info

--- gst/mpegtsmux/tsmux/tsmux.c
+++ gst/mpegtsmux/tsmux/tsmux.c
@@ -1019,9 +1019,11 @@ tsmux_write_stream_packet (TsMux * mux, TsMuxStream * stream)
     }
 
     /* Need to decide whether to write a new PCR in this packet */
-    if (stream->last_pcr == -1 ||
-        (cur_pcr - stream->last_pcr >
-            (TSMUX_SYS_CLOCK_FREQ / TSMUX_DEFAULT_PCR_FREQ))) {
+   // if (stream->last_pcr == -1 ||
+    //    (cur_pcr - stream->last_pcr >
+    //        (TSMUX_SYS_CLOCK_FREQ / TSMUX_DEFAULT_PCR_FREQ))) 
+  if(cur_pcr - stream->last_pcr !=0)  // wenfeng
+   {
 
       stream->pi.flags |=
           TSMUX_PACKET_FLAG_ADAPTATION | TSMUX_PACKET_FLAG_WRITE_PCR;
